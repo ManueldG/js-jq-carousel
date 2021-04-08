83a9cc97fc6e39f84889f5dd26e99dae232fb294
Manuel della Gala <manuel.dellagala@gmail.com>
2021-04-08 23:16:14 +0200

    ok

--- main.js
+++ main.js
@@ -13,33 +13,31 @@ Clicchiamo sui pallini e mostriamo l’immagine corrispondente
 Consiglio
 Come sempre è importante usare la documentazione di jQuery per scoprire/rinfrescare la memoria sulle funzioni che potrebbero esserci utili.*/
 
-$(document).ready(function(){
+$(document).ready(function(){ // onload al caricamento della pagina avvio la funzione principale 
 
-    $(document).keydown(function(e){
-        if (e.originalEvent.keyCode===39)
-            next();
-        if (e.originalEvent.keyCode===37)
+    $(document).keydown(function(e){ // legge l0evento keydown
+        if (e.originalEvent.keyCode===39) // nel caso premo direzione destra chiamo la funzione next()
+            next(); 
+        if (e.originalEvent.keyCode===37) // se premo direzione sinistra chiamo la funzione prev()
             prev();
     })
 
-    $(".next").click(function(){
+    $(".next").click(function(){ // se clicco sulla classe next chiama la funzione next()
         next();        
     });
 
-    $( ".prev").click(function(){
+    $( ".prev").click(function(){ // se clicco sulla classe prev chiama la funzione prev()
         prev();        
     });
 
-    $('.fa-circle').click(function(){
+    $('.fa-circle').click(function(){ // se clicco sulla classe fa-circle chiama la funzione anonima
         
-        $(".active").removeClass("active");
-        $(this).addClass("active");
-        var index = ($(this).index()+1);
-        var image= $("img:nth-of-type("+index+")")/*.get(index) */;
-        
-        //$('img').get($(this).index()).addClass("active");
-        console.log(image);
-        image.addClass("active");
+        $(".active").removeClass("active"); // rimuove la classe active
+        $(this).addClass("active"); // aggiunge la classe active all'elemento cliccato
+        var index = ($(this).index()+1); // restituisce l'index dell'elemento cliccato 
+        var image= $("img:nth-of-type("+index+")");  // identifica l'img in posizione index 
+        console.log(image); // stampo in console l'oggetto image
+        image.addClass("active"); // aggiungo la classe active all'oggetto image
         
     })
 
@@ -48,32 +46,32 @@ $(document).ready(function(){
 
 function next(){
     console.log($(".active")[0].className);
-    if (!($(".active")[0].className).includes("last"))
+    if (!($(".active")[0].className).includes("last")) // se nelle classi non c'è la classe last 
         {
-            $(".active").removeClass("active").next().addClass("active");
+            $(".active").removeClass("active").next().addClass("active"); // rimuovo la classe active e aggiungo la classe active all'elemento successivo
         }
     
     else
         {
-            console.log($('.last'));
-            $(".last").removeClass("active");
-            $(".first").addClass("active");
+            console.log($('.last')); // altrimenti 
+            $(".last").removeClass("active"); // rimuovo la classe active dalla classe last
+            $(".first").addClass("active"); // e aggiungo la classe active alla classe first
         }
 }
 
 
 function prev(){
     console.log($(".active")[0].className);
-    if (!($(".active")[0].className).includes("first"))
+    if (!($(".active")[0].className).includes("first")) // se nelle classi non c'è la classe first    
         {
-            $(".active").removeClass("active").prev().addClass("active");
+            $(".active").removeClass("active").prev().addClass("active");// rimuovo la classe active e aggiungo la classe active all'elemento successivo
         }
         
-    else
+    else// altrimenti
         {
             console.log($('.last'));
-            $(".first").removeClass("active");
-            $(".last").addClass("active");
+            $(".first").removeClass("active");// rimuovo la classe active dalla classe first
+            $(".last").addClass("active");// e aggiungo la classe active alla classe last
         }
 
 }
